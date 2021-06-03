8ef644f8176fb0cf92d168c1eaa3b0dd44ff07aa
Peter Kin <kinpeter85@gmail.com>
2021-06-03 07:38:52 +0200

    Initialize MongoDB in docker

---
+++ README.md
@@ -0,0 +1,2 @@
+# Startpage v3
+
---
+++ dev-db/mongo-init.js
@@ -0,0 +1,18 @@
+db.createUser(
+	{
+		  user: "pkstartdev",
+		  pwd: "pkstartdev",
+		  roles: [
+		{
+			  role: "readWrite",
+			  db: "pkstartdev"
+		}
+		  ]
+	}
+);
+
+db= db.getSiblingDB('pkstartdev')
+
+db.createCollection('init')
+
+db.getCollection('init').insertOne({ initialized: new Date() })
