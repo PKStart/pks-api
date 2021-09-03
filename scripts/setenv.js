60071d606c5a1d0242dd61880cc81719774d1d45
Peter Kin <kinpeter85@gmail.com>
2021-09-03 10:18:41 +0200

    Cleanup

--- scripts/setenv.js
+++ scripts/setenv.js
@@ -4,15 +4,12 @@ require("dotenv").config({ path: ".env" });
 
 const environments = ["DEV", "PROD"];
 
-const variables = ["PK_TEST", "PK_API_URL"];
+const variables = ["PK_API_URL"];
 
 const paths = {
   DEV: "./apps/main/src/environments/environment.ts",
   PROD: "./apps/main/src/environments/environment.prod.ts",
 };
-
-// PK_TEST: '${process.env[isProd ? "PK_TEST_PROD" : "PK_TEST_DEV"]}',
-//   PK_API_URL: '${process.env[isProd ? "PK_API_URL_PROD" : "PK_API_URL_DEV"]}'
 
 environments.forEach((env) => {
   const isProd = env === "PROD";
@@ -28,10 +25,7 @@ ${variableList}
   `;
   writeFile(paths[env], content, (err) => {
     if (err) {
-      console.log(
-        `[setenv] Error while setting frontend ${env} environment variables:`,
-        err
-      );
+      console.log(`[setenv] Error while setting frontend ${env} environment variables:`, err);
     }
     console.log(`[setenv] Wrote ${env} environment variables to ${paths[env]}`);
   });
