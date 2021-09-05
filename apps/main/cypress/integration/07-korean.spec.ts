d320796d1a4f5a4b7943140e35666cd841ac84eb
Peter Kin <kinpeter85@gmail.com>
2021-09-05 17:56:45 +0200

    Add remaining e2e test

--- apps/main/cypress/integration/07-korean.spec.ts
+++ apps/main/cypress/integration/07-korean.spec.ts
@@ -1,14 +1,14 @@
-import { weatherPo as po } from '../support/page-objects/weather.po'
+import { koreanPo as po } from '../support/page-objects/korean.po'
 import { appBarPo } from '../support/page-objects/main-layout.po'
 
-describe('Weather', () => {
+describe('Korean', () => {
   beforeEach(() => {
     cy.login()
   })
 
   it('Should inform the user that the service is not available', () => {
-    appBarPo.weatherBtn().click()
-    po.box().should('be.visible').should('contain', 'Weather service is not available.')
+    appBarPo.koreanBtn().click()
+    po.box().should('be.visible').should('contain', 'Korean word list is not available.')
     po.actions().should('have.length', 2)
     po.syncBtn().should('be.disabled')
   })
--- apps/main/cypress/integration/08-birthdays.spec.ts
+++ apps/main/cypress/integration/08-birthdays.spec.ts
@@ -1,14 +1,14 @@
-import { weatherPo as po } from '../support/page-objects/weather.po'
+import { birthdaysPo as po } from '../support/page-objects/birthdays.po'
 import { appBarPo } from '../support/page-objects/main-layout.po'
 
-describe('Weather', () => {
+describe('Birthdays', () => {
   beforeEach(() => {
     cy.login()
   })
 
   it('Should inform the user that the service is not available', () => {
-    appBarPo.weatherBtn().click()
-    po.box().should('be.visible').should('contain', 'Weather service is not available.')
+    appBarPo.birthdaysBtn().click()
+    po.box().should('be.visible').should('contain', 'Birthdays are not available.')
     po.actions().should('have.length', 2)
     po.syncBtn().should('be.disabled')
   })
